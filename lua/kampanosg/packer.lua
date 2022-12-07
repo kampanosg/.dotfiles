8bf834df426517c885e2d857378c4c83c8bae1fd
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-07 11:01:10 +0000

    made vim better for rust

--- lua/kampanosg/packer.lua
+++ lua/kampanosg/packer.lua
@@ -14,10 +14,28 @@ return require('packer').startup(function(use)
     requires = 'kyazdani42/nvim-web-devicons'
   } 
 
---  use {
---    'nvim-telescope/telescope.nvim', tag = '0.1.x',
---    requires = { {'nvim-lua/plenary.nvim'} }
---  }
+
+  use 'williamboman/mason.nvim'    
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'neovim/nvim-lspconfig' 
+  --use 'nvim-treesitter/nvim-treesitter'
+
+  -- Completion framework:
+  use 'hrsh7th/nvim-cmp' 
+
+  -- LSP completion source:
+  use 'hrsh7th/cmp-nvim-lsp'
+
+  -- Useful completion sources:
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
+  use 'hrsh7th/cmp-vsnip'                             
+  use 'hrsh7th/cmp-path'                              
+  use 'hrsh7th/cmp-buffer'                            
+  use 'hrsh7th/vim-vsnip' 
+  
+  -- rust
+  use 'simrat39/rust-tools.nvim'
 
   use { 'junegunn/fzf', run = ":call fzf#install()" }
   use { 'junegunn/fzf.vim' }
