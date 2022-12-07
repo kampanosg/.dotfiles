a7680b595b2042f3c813e15f397c1f4785e5f2a7
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-07 01:43:05 +0000

    added git blame

---
+++ after/plugin/gitsigns.lua
@@ -0,0 +1,3 @@
+require('gitsigns').setup()
+
+vim.cmd("Gitsigns toggle_current_line_blame")
--- lua/kampanosg/packer.lua
+++ lua/kampanosg/packer.lua
@@ -5,6 +5,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use 'mhinz/vim-startify'
+  use 'lewis6991/gitsigns.nvim'
 
   use {
     'kyazdani42/nvim-tree.lua',
