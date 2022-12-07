def8f48b2573fc630213455413aef4b8137aa9c8
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-07 15:27:13 +0000

    better buffers

--- lua/kampanosg/packer.lua
+++ lua/kampanosg/packer.lua
@@ -28,6 +28,11 @@ return require('packer').startup(function(use)
     requires = "kyazdani42/nvim-web-devicons",
   }
 
+  use {
+    'akinsho/bufferline.nvim',
+    tag = "v3.*",
+    requires = 'nvim-tree/nvim-web-devicons'
+  }
 
   -- Completion framework:
   use 'hrsh7th/nvim-cmp' 
--- lua/kampanosg/remap.lua
+++ lua/kampanosg/remap.lua
@@ -4,7 +4,7 @@ local nnoremap = require("kampanosg.keymap").nnoremap
 nnoremap('<leader>tb', '<cmd>ToggleTerm<CR>')
 
 -- NvimTree
-nnoremap("<leader>1", "<cmd>NvimTreeToggle<CR>")
+nnoremap("<leader>t1", "<cmd>NvimTreeToggle<CR>")
 nnoremap("<leader>tf", "<cmd>NvimTreeFocus<CR>")
 nnoremap("<leader>l", "<cmd>NvimTreeFindFile<CR>")
 
@@ -30,3 +30,14 @@ nnoremap('gc', '<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
 nnoremap('gR', '<cmd>lua vim.lsp.buf.rename()<CR>')
 nnoremap('ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
 
+-- Buffers
+nnoremap('<leader>1', '<Cmd>BufferLineGoToBuffer 1<CR>')
+nnoremap('<leader>2', '<Cmd>BufferLineGoToBuffer 2<CR>')
+nnoremap('<leader>3', '<Cmd>BufferLineGoToBuffer 3<CR>')
+nnoremap('<leader>4', '<Cmd>BufferLineGoToBuffer 4<CR>')
+nnoremap('<leader>5', '<Cmd>BufferLineGoToBuffer 5<CR>')
+nnoremap('<leader>6', '<Cmd>BufferLineGoToBuffer 6<CR>')
+nnoremap('<leader>7', '<Cmd>BufferLineGoToBuffer 7<CR>')
+nnoremap('<leader>8', '<Cmd>BufferLineGoToBuffer 8<CR>')
+nnoremap('<leader>9', '<Cmd>BufferLineGoToBuffer 9<CR>')
+nnoremap('<leader>$', '<Cmd>BufferLineGoToBuffer -1<CR>')
