0300791a0715c29766c9264cf80a90563ce00ac7
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-09 10:31:41 +0000

    cleanup and add the basics

--- lua/kampanosg/packer.lua
+++ lua/kampanosg/packer.lua
@@ -1,44 +1,15 @@
-vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
 
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
   -- Editor
-  use { 'junegunn/fzf', run = ":call fzf#install()" }
-  use 'junegunn/fzf.vim'
-  use 'feline-nvim/feline.nvim'
-  use 'yamatsum/nvim-cursorline'
+  use('tpope/vim-fugitive')
+  use("nvim-lua/plenary.nvim")
   use {
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons'
-  }
-  use {
-    'akinsho/bufferline.nvim',
-    tag = "v3.*",
-    requires = 'nvim-tree/nvim-web-devicons'
-  }
-
-
-  -- Coding
-  use 'lewis6991/gitsigns.nvim'
---  use 'yamatsum/nvim-cursorline'
-  use {
-    "ThePrimeagen/refactoring.nvim",
-    requires = {
-      {"nvim-lua/plenary.nvim"},
-      {"nvim-treesitter/nvim-treesitter"}
-    }
-  }
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-
-
-  -- Languages
-  use {'neoclide/coc.nvim', branch = 'release'}
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  } 
 
   -- Theme
   use 'navarasu/onedark.nvim'
