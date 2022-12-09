e77d6898eeb13ba5da1d9b0a28c8c5b09ae86cfc
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-09 13:03:26 +0000

    lsp

--- lua/kampanosg/packer.lua
+++ lua/kampanosg/packer.lua
@@ -26,6 +26,20 @@ return require('packer').startup(function(use)
   }
 
 
+  -- Coding
+  use("neovim/nvim-lspconfig")
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/nvim-cmp")
+  use("tzachar/cmp-tabnine", { run = "./install.sh" })
+  use("onsails/lspkind-nvim")
+  use("nvim-lua/lsp_extensions.nvim")
+  use("glepnir/lspsaga.nvim")
+  use("simrat39/symbols-outline.nvim")
+  use("L3MON4D3/LuaSnip")
+  use("saadparwaiz1/cmp_luasnip")
+
+
   -- Theme
   use('navarasu/onedark.nvim')
   -- use('rose-pine/neovim')
