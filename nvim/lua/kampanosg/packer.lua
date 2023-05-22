3d57dfd619f668653f041daa7dc3829141f70af0
George Kampanos <george.kampanos@nandos.co.uk>
2023-05-22 10:58:52 +0100

    show trailing whitespace

--- nvim/lua/kampanosg/packer.lua
+++ nvim/lua/kampanosg/packer.lua
@@ -30,6 +30,16 @@ return require('packer').startup(function(use)
         }
     }
     use { "folke/trouble.nvim", requires = "nvim-tree/nvim-web-devicons" }
+    use {
+        'johnfrankmorgan/whitespace.nvim',
+        config = function ()
+            require('whitespace-nvim').setup({
+                highlight = 'DiffDelete',
+                ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'help' },
+                ignore_terminal = true,
+            })
+        end
+    }
 
 
     -- LSP
