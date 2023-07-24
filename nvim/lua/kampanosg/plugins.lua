6b1eaab01b50d8d73a2260982007146bd111af64
George Kampanos <george.kampanos@nandos.co.uk>
2023-07-24 01:12:45 +0100

    better buffer plugin

--- nvim/lua/kampanosg/plugins.lua
+++ nvim/lua/kampanosg/plugins.lua
@@ -73,46 +73,26 @@ require('lazy').setup({
             require('lualine').setup()
         end
     },
-    {
-        'akinsho/bufferline.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function ()
-            require("bufferline").setup{
-                options = {
-                    mode = 'buffers',
-                    numbers = 'ordinal',
-                    offsets = {
-                        {
-                            filetype = "NvimTree",
-                            text = "File Explorer",
-                            text_align = "center",
-                            separator = false
-                        }
-                    },
-                    diagnostics = "nvim_lsp",
-                    diagnostics_indicator = function(_count, _level, diagnostics_dict, _context)
-                        local s = " "
-                        for e, n in pairs(diagnostics_dict) do
-                            local sym = e == "error" and " " or (e == "warning" and " " or "" )
-                            s = s .. n .. sym
-                        end
-                        return s
-                    end
+    {'romgrk/barbar.nvim',
+        dependencies = {
+            'lewis6991/gitsigns.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
+        init = function() vim.g.barbar_auto_setup = false end,
+        opts = {
+            sidebar_filetypes = {
+                NvimTree = true,
+            },
+            icons = {
+                diagnostics = {
+                    [vim.diagnostic.severity.ERROR] = {enabled = true, icon = 'ﬀ'},
+                    [vim.diagnostic.severity.WARN] = {enabled = false},
+                    [vim.diagnostic.severity.INFO] = {enabled = false},
+                    [vim.diagnostic.severity.HINT] = {enabled = true},
                 }
-            }
-        end,
-        keys = {
-            { '<leader>1', '<cmd>BufferLineGoToBuffer 1<cr>' },
-            { '<leader>2', '<cmd>BufferLineGoToBuffer 2<cr>' },
-            { '<leader>3', '<cmd>BufferLineGoToBuffer 3<cr>' },
-            { '<leader>4', '<cmd>BufferLineGoToBuffer 4<cr>' },
-            { '<leader>5', '<cmd>BufferLineGoToBuffer 5<cr>' },
-            { '<leader>6', '<cmd>BufferLineGoToBuffer 6<cr>' },
-            { '<leader>7', '<cmd>BufferLineGoToBuffer 7<cr>' },
-            { '<leader>8', '<cmd>BufferLineGoToBuffer 8<cr>' },
-            { '<leader>9', '<cmd>BufferLineGoToBuffer 9<cr>' },
-            { '<leader>$', '<cmd>BufferLineGoToBuffer -1<cr>' },
-        }
+            },
+        },
+        version = '^1.0.0',
     },
     {
         'yamatsum/nvim-cursorline',
--- nvim/lua/kampanosg/remap.lua
+++ nvim/lua/kampanosg/remap.lua
@@ -1,11 +1,35 @@
-local Remap = require("kampanosg.keymap")
-local nnoremap = Remap.nnoremap
-local vnoremap = Remap.vnoremap
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
 
 -- Move a line of code up or down
-nnoremap("<leader>mj", "<cmd>m +1<CR>")
-nnoremap("<leader>mk", "<cmd>m -2<CR>")
-vnoremap("<leader>mj", "<cmd>m +1<CR>")
-vnoremap("<leader>mk", "<cmd>m -2<CR>")
+map('n', '<leader>mj', '<cmd>m +1<CR>', opts)
+map('n', '<leader>mk', '<cmd>m -2<CR>', opts)
+map('n', '<leader>mj', '<cmd>m +1<CR>', opts)
+map('n', '<leader>mk', '<cmd>m -2<CR>', opts)
 
-nnoremap('<leader>dl', '<cmd>:t.<CR>') -- Duplicate line
+map('n', '<leader>dl', '<cmd>:t.<CR>', opts) -- Duplicate line
+
+
+-- barbar
+
+-- Move to previous/next
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+
+-- Goto buffer in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+
+-- Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+
+-- Close buffer
+map('n', '<A-w>', '<Cmd>BufferClose<CR>', opts)
