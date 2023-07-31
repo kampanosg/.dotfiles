05946704a36f783c387298968e7e5cfbd7f5a1b3
George Kampanos <george.kampanos@nandos.co.uk>
2023-07-31 18:35:26 +0100

    changed the mappings for barbar

--- nvim/lua/kampanosg/remap.lua
+++ nvim/lua/kampanosg/remap.lua
@@ -12,17 +12,17 @@ map('n', '<leader>dl', '<cmd>:t.<CR>', opts) -- Duplicate line
 -- barbar
 
 -- goto buffer in position...
-map('n', '<S-1>', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<S-2>', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<S-3>', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<S-4>', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<S-5>', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<S-6>', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<S-7>', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<S-8>', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<S-9>', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<S-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<leader>1', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<leader>2', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<leader>3', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<leader>4', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<leader>5', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<leader>6', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<leader>7', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<leader>8', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<leader>9', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<leader>0', '<Cmd>BufferLast<CR>', opts)
 
 map('n', '<S-w>', '<Cmd>BufferClose<CR>', opts) -- close buffer
-map('n', '<S-,>', '<Cmd>BufferPrevious<CR>', opts) -- move to prev buffer
-map('n', '<S-.>', '<Cmd>BufferNext<CR>', opts) -- move to next buffer
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts) -- move to prev buffer
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts) -- move to next buffer
