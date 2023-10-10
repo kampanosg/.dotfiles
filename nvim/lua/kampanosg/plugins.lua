bdd1946c43f3aa5020527c06def0bd32cd4ed345
George Kampanos <george.kampanos@nandos.co.uk>
2023-10-10 18:41:18 +0300

    added wilder

--- nvim/lua/kampanosg/plugins.lua
+++ nvim/lua/kampanosg/plugins.lua
@@ -108,6 +108,23 @@ require('lazy').setup({
             }
         end
     },
+    {
+        'gelguy/wilder.nvim',
+        config = function()
+            local wilder = require('wilder')
+            wilder.setup({modes = {':', '/', '?'}})
+            wilder.set_option('use_python_remote_plugin', 0)
+            wilder.set_option('pipeline', {
+                wilder.branch(
+                    wilder.cmdline_pipeline(),
+                    wilder.search_pipeline()
+                ),
+            })
+            wilder.set_option('renderer', wilder.popupmenu_renderer({
+                highlighter = wilder.basic_highlighter(),
+            }))
+        end,
+    },
 
 
     -- coding
