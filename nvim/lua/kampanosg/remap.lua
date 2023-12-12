cbb0111ba2656cbba519705ff08460d3313006fb
George Kampanos <george.kampanos@nandos.co.uk>
2023-12-12 00:51:25 +0000

    mapping to keep paste register after pasting

--- nvim/lua/kampanosg/remap.lua
+++ nvim/lua/kampanosg/remap.lua
@@ -5,25 +5,11 @@ local opts = { noremap = true, silent = true }
 map('n', '<leader>mj', '<cmd>m +1<CR>', opts)
 map('n', '<leader>mk', '<cmd>m -2<CR>', opts)
 
-map('n', '<leader>dl', '<cmd>:t.<CR>', opts) -- Duplicate line
-
--- barbar
-
--- goto buffer in position...
-map('n', '<leader>1', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<leader>2', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<leader>3', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<leader>4', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<leader>5', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<leader>6', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<leader>7', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<leader>8', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<leader>9', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<leader>0', '<Cmd>BufferLast<CR>', opts)
-
-map('n', '<S-w>', '<Cmd>BufferClose<CR>', opts) -- close buffer
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts) -- move to prev buffer
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts) -- move to next buffer
+-- Duplicate line
+map('n', '<leader>dl', '<cmd>:t.<CR>', opts)
 
 -- copilot, change accept sequence
 vim.api.nvim_set_keymap('i', '<C-j>', 'copilot#Accept("<CR>")', { expr = true, silent = true})
+
+-- keep the paste register
+map('x', '<leader>p', '"_dp', opts)
