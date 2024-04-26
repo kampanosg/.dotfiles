ebeff652c2a08a79b66b18207b21ad04badc5ae6
George Kampanos <george.kampanos@nandos.co.uk>
2024-04-26 13:46:56 +0100

    wezterm changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,18 @@
 local wezterm = require 'wezterm'
 
 return {
-  window_padding = { top = 0, bottom = 0, left = 0, right = 0 },
-  window_background_opacity = 0.90,
-  macos_window_background_blur = 20,
+  window_padding = { top = 15, bottom = 0, left = 0, right = 0 },
+  window_background_opacity = 0.80,
+  macos_window_background_blur = 25,
+  -- font = wezterm.font({
+  --   family = 'Geist Mono',
+  -- }),
   font = wezterm.font({
-    family = "Geist Mono",
+      family = 'Liga SFMono Nerd Font',
   }),
-  font_size = 17.5,
-  color_scheme = "Abernathy",
+  harfbuzz_features = {"calt=0", "clig=0", "liga=0"}, -- disable ligature
+  font_size = 18.5,
+  color_scheme = 'Navy and Ivory (terminal.sexy)',
   use_fancy_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
   send_composed_key_when_left_alt_is_pressed = true,
