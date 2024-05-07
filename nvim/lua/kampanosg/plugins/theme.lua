9587724a44babada6e0ea5cf4f861eb2b72d2ccd
George Kampanos <george.kampanos@nandos.co.uk>
2024-05-07 16:17:56 +0100

    cleanup theme

--- nvim/lua/kampanosg/plugins/theme.lua
+++ nvim/lua/kampanosg/plugins/theme.lua
@@ -4,38 +4,18 @@ return {
     priority = 1000,
     config = function()
         require('catppuccin').setup({
-            flavour = 'macchiato',
+            flavour = 'mocha',
             transparent_background = true,
             integrations = {
-                dap = true,
-                dap_ui = true,
                 fidget = true,
                 gitsigns = true,
                 mason = true,
                 neotest = true,
                 cmp = true,
                 ufo = true,
-                telescope = {
-                    enabled = true,
-                },
-                indent_blankline = {
-                    enabled = true,
-                    scope_color = 'peach',
-                    colored_indent_levels = false,
-                },
                 treesitter_context = true,
-                lsp_trouble = true,
-                illuminate = {
-                    enabled = true,
-                    lsp = false
-                },
             },
         })
         vim.cmd.colorscheme 'catppuccin'
-
-        local sign = vim.fn.sign_define
-        sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
-        sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = ""})
-        sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = ""})
     end
 }
