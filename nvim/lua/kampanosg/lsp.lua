af0ff27af8ebf77963110def6e5ed81877a67ea6
George Kampanos <george.kampanos@nandos.co.uk>
2024-07-24 13:06:08 +0300

    toggle lsp hints

--- nvim/lua/kampanosg/lsp.lua
+++ nvim/lua/kampanosg/lsp.lua
@@ -70,5 +70,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<F3>', function() vim.diagnostic.goto_prev() end, opts)
         vim.keymap.set('n', '<F4>', function() vim.diagnostic.open_float() end, opts)
         vim.keymap.set('n', '<leader>g==', function() vim.lsp.buf.format { async = false } end, opts)
+
+        if vim.lsp.inlay_hint then
+            vim.keymap.set('n', '<leader>L',
+                function()
+                    if vim.lsp.inlay_hint.is_enabled() then
+                        vim.lsp.inlay_hint.enable(false, { event.buf })
+                    else
+                        vim.lsp.inlay_hint.enable(true, { event.buf })
+                    end
+                end, opts)
+        end
     end
 })
