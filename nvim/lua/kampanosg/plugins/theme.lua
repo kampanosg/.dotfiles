4bf4870515aa0bb208faad1d9a6636390f3d5505
George Kampanos <gkabanos@icloud.com>
2024-12-28 16:41:31 +0000

    visual highlight colour

--- nvim/lua/kampanosg/plugins/theme.lua
+++ nvim/lua/kampanosg/plugins/theme.lua
@@ -1,7 +1,7 @@
 return {
     'EdenEast/nightfox.nvim',
     config = function()
-        vim.cmd('hi Visual guifg=#2c3e50 guibg=#e67e22 gui=none')
         vim.cmd("colorscheme carbonfox")
+        vim.cmd('hi Visual guifg=#2c3e50 guibg=#e67e22 gui=none')
     end,
 }
